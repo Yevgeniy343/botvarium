4414460a067e8c3244ca170088479dba8e0f6e1f
Yevgeniy343 <rt4387@yahoo.com>
2023-02-06 16:28:39 +0300

    verv

--- src/Components/Services/WebApp.js
+++ src/Components/Services/WebApp.js
@@ -1,20 +1,21 @@
 import Wrapper from "./wr-WebApp";
 import img from "../../assets/imgs/undraw_Web_devices_re_m8sc.png";
-import { Typewriter } from "react-simple-typewriter";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const WebApp = () => {
+  const [text] = useTypewriter({
+    words: [
+      "Веб-приложения можно разделить на несколько типов,в зависимости от разных сочетаний его основных составляющих ",
+    ],
+    loop: {},
+    typeSpeed: 100,
+  });
   return (
     <Wrapper>
       <div className="content">
         <div className="codeEditor">
-          <p>Лапутька</p>
-          <Typewriter
-            words={["Жопчик", "Пельмешка", "Лапатутька"]}
-            cursor
-            cursorStyle="_"
-            typeSpeed={100}
-            loop
-          />
+          <span>{text}</span>
+          {/* <Cursor cursorColor="b45af2" /> */}
         </div>
         <div className="image">
           <img src={img} alt="web" />
